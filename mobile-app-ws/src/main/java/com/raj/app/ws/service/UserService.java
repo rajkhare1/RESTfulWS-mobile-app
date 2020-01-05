84b6322c9ed62a5a1f84f5a098169a4ba5b3057b
rajkhare <rajkhare@192.168.31.81>
2020-01-05 21:06:52 +0530

    Update for BeanUtils

---
+++ mobile-app-ws/src/main/java/com/raj/app/ws/service/UserService.java
@@ -0,0 +1,8 @@
+package com.raj.app.ws.service;
+
+import com.raj.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+	UserDto createUser(UserDto user);
+
+}
---
+++ mobile-app-ws/src/main/java/com/raj/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.raj.app.ws.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.raj.app.ws.service.UserService;
+import com.raj.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
